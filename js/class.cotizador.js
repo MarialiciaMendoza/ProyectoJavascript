bea1457a9963e645ff15797596aa7f23357a47fa
Marialicia Mendoza <marialiciamendoza@gmail.com>
2023-11-18 19:30:42 -0300

    cambios

--- js/class.cotizador.js
+++ js/class.cotizador.js
@@ -6,18 +6,3 @@ class Cotizador {
         return this.carrito.reduce((acumulador, servicio) => acumulador + servicio.precio * servicio.muestras, 0);
     }
 }
-
-// if (cantidad !== null && !isNaN && cantidad > 0){
-//     const total = precio * parseInt(cantidad)
-// }
-
-// class Compra { // sustantivo, singular, palabra simple, instanciable 
-//     constructor(carritoDeCompras) {
-//         this.carrito = carritoDeCompras
-//     }
-//     obtenerSubtotal() {
-//         if (this.carrito.length > 0) { //función de orden superior REDUCE()
-//             return this.carrito.reduce((acumulador, prenda)=> acumulador + prenda.precio, 0)
-//         }
-//     }
-// }
