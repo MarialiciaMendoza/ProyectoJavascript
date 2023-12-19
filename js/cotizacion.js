350072e57443fc8d9c923332a6481d6d9589d0bf
Marialicia Mendoza <marialiciamendoza@gmail.com>
2023-12-19 18:49:03 -0300

    agregando librerias y funcionalidades al formulario

--- js/cotizacion.js
+++ js/cotizacion.js
@@ -2,6 +2,7 @@ const spanServicio = document.querySelector("span.label-servicio")
 const spanCantidad = document.querySelector("span.label-cantidad")
 const spanPrecio = document.querySelector("span.label-precio")
 const spanTotalDevolver = document.querySelector("span.label-total")
+const inputEnviar = document.querySelector("input#enviar")
 
 function recuperarDeLS() {
     const datosDeCotizacion = JSON.parse(localStorage.getItem("DatosDeCotizacion"))
@@ -13,3 +14,39 @@ function recuperarDeLS() {
 }
 
 recuperarDeLS()
+
+inputEnviar.addEventListener("click", (event) => {
+    event.preventDefault();
+
+    // Obtener los valores de los campos del formulario
+    const nombre = document.querySelector("input[name='name']").value.trim();
+    const correo = document.querySelector("input[name='email']").value.trim();
+    const telefono = document.querySelector("input[name='telefono']").value.trim();
+    const comentarios = document.querySelector("#comentarios").value.trim();
+
+    // Verificar si todos los campos están llenos
+    if (nombre && correo && telefono && comentarios) {
+        // Mostrar el mensaje de éxito y restablecer el formulario
+        Swal.fire({
+            title: "Formulario enviado correctamente",
+            confirmButtonText: "Cerrar",
+            icon: "success",
+            color: "green",
+        }).then(() => {
+            // Restablecer los valores de los campos del formulario
+            document.querySelector("input[name='name']").value = "";
+            document.querySelector("input[name='email']").value = "";
+            document.querySelector("input[name='telefono']").value = "";
+            document.querySelector("#comentarios").value = "";
+        });
+    } else {
+        // Mostrar el mensaje de error si algún campo está vacío
+        Swal.fire({
+            title: "Error",
+            text: "Por favor, completa todos los campos del formulario.",
+            confirmButtonText: "Cerrar",
+            icon: "error",
+            color: "red",
+        });
+    }
+});
--- js/index.js
+++ js/index.js
@@ -2,6 +2,7 @@
 const selectServicio = document.querySelector("#selectServicio")
 const inputCantidad = document.querySelector("#cantidadMuestras")
 const btnCotiza = document.querySelector("#btnCotiza")
+const inputEnviar = document.querySelector("input#enviar")
 
 const servicios = [{id: 1, descripcion: 'Analisis microbiológico', precio: 35, cantidad :0},
                    {id: 2, descripcion: 'Analisis fisico-quimico', precio: 50, cantidad :0},
@@ -39,7 +40,42 @@ function cotizarServicio() {
 
     location.href = "contacto.html"
 }
+cargarServicios()
 
 btnCotiza.addEventListener("click", cotizarServicio)
 
-cargarServicios()
+inputEnviar.addEventListener("click", (event) => {
+    event.preventDefault();
+
+    // Obtener los valores de los campos del formulario
+    const nombre = document.querySelector("input[name='name']").value.trim();
+    const correo = document.querySelector("input[name='email']").value.trim();
+    const telefono = document.querySelector("input[name='telefono']").value.trim();
+    const comentarios = document.querySelector("#comentarios").value.trim();
+
+    // Verificar si todos los campos están llenos
+    if (nombre && correo && telefono && comentarios) {
+        // Mostrar el mensaje de éxito y restablecer el formulario
+        Swal.fire({
+            title: "Formulario enviado correctamente",
+            confirmButtonText: "Cerrar",
+            icon: "success",
+            color: "green",
+        }).then(() => {
+            // Restablecer los valores de los campos del formulario
+            document.querySelector("input[name='name']").value = "";
+            document.querySelector("input[name='email']").value = "";
+            document.querySelector("input[name='telefono']").value = "";
+            document.querySelector("#comentarios").value = "";
+        });
+    } else {
+        // Mostrar el mensaje de error si algún campo está vacío
+        Swal.fire({
+            title: "Error",
+            text: "Por favor, completa todos los campos del formulario.",
+            confirmButtonText: "Cerrar",
+            icon: "error",
+            color: "red",
+        });
+    }
+});
